39915369245e551675c70ee4c4ef7c3859fa1617
Andrew Stearns <andrew.stearns@sciencespo.fr>
2018-05-14 14:27:26 +0200

    The answer function works

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,5 +3,16 @@ class QuestionsController < ApplicationController
   end
 
   def answer
+    @question = params[:question]
+    if @question == "I am going to work"
+      @answer = "Great!"
+    elsif @question.include?("?")
+      @answer = "Silly question, get dressed and go to work!"
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
   end
 end
+
+# This works for the test links, because I define @question here as a value of the
+# query string that is in the URL.
